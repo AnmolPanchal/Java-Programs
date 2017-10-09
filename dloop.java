37b160325a07892c79284849f6742a384a17ac07
Anmol Panchal <32629040+AnmolPanchal@users.noreply.github.com>
2017-10-09 01:32:39 -0400

    Do while loop Example

---
+++ dloop.java
@@ -0,0 +1,18 @@
+class dloop
+{
+
+  public static void main(String[] ap)
+{
+     int x = 10;
+
+ do     	
+{
+	System.out.println("value of x " + x);
+	 x++;
+	}
+	
+	while( x < 30);
+	
+ System.out.println("x ="+ x);
+}
+}
